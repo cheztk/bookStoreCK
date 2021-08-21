328c9e727c9b01d402ac4abaab5e84e0d8d82024
Chezt <ckengchon@gmail.com>
2021-08-21 14:31:30 +0700

    Initial setup

---
+++ src/main/java/com/wecode/bookStoreCK/BookStoreCkApplication.java
@@ -0,0 +1,13 @@
+package com.wecode.bookStoreCK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreCkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreCkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wecode/bookStoreCK/BookStoreCkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wecode.bookStoreCK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreCkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
