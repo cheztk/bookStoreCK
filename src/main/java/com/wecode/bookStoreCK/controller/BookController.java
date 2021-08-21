9bc9236b8a838bf85ddc88ec7e0502d2bcae7b3a
Chezt <ckengchon@gmail.com>
2021-08-21 14:59:49 +0700

    correct DTO path

--- src/main/java/com/wecode/bookStoreCK/controller/BookController.java
+++ src/main/java/com/wecode/bookStoreCK/controller/BookController.java
@@ -1,6 +1,6 @@
 package com.wecode.bookStoreCK.controller;
 
-import com.weCode.bookStore.dto.BookDto;
+import com.wecode.bookStoreCK.dto.BookDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
