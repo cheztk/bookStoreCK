ee625dc5689ecffc1704006aa517189fdca279cf
Chezt <ckengchon@gmail.com>
2021-08-30 19:59:12 +0700

    cross origin correction

--- src/test/java/com/wecode/bookStoreCK/config/CrossOriginConfig.java
+++ src/test/java/com/wecode/bookStoreCK/config/CrossOriginConfig.java
@@ -2,11 +2,14 @@ package com.wecode.bookStoreCK.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CrossOriginConfig implements WebMvcConfigurer {
 
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
