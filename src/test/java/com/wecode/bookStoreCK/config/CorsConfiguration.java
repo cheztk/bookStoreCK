b8494b7e783da5b41cfd531140306a92e0159ba4
Chezt <ckengchon@gmail.com>
2021-08-30 20:23:49 +0700

    cross origin config adjustmemt

---
+++ src/test/java/com/wecode/bookStoreCK/config/CorsConfiguration.java
@@ -0,0 +1,25 @@
+package com.wecode.bookStoreCK.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration
+{
+
+
+   @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://book-store-web-ck.herokuapp.com/");
+            }
+        };
+    }
+}
